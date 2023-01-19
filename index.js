3a8edb1bcfd8c4aa371f6b184f0ab7d9c3806f64
Aleksander <sanyok.10rus@gmail.com>
2023-01-19 22:22:13 +0300

    routing node.js

--- index.js
+++ index.js
@@ -9,18 +9,43 @@ const server = http.createServer((req, res) => {
 
   res.setHeader('Content-Type', 'text/html');
 
-  if (req.url = '/') {
-    fs.readFile('./views/HomePage.html', (err, data) => {
+  const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+
+  let basePath = '';
+
+  switch(req.url) {
+    case '/':
+    case '/home':
+    case '/index.html':
+      basePath = createPath('HomePage');
+      res.statusCode = 200;
+      break;
+    case '/contacts':
+      basePath = createPath('Contacts');
+      res.statusCode = 200;
+      break;
+    case '/about':
+      res.statusCode = 301;
+      res.setHeader('Location', '/contacts');
+      res.end();
+      break;
+    default:
+      basePath = createPath('NotFoundPage');
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(basePath, (err, data) => {
       if (err) {
         console.log(err);
+        res.statusCode = 500;
         res.end();
       }
       else {
         res.write(data);
         res.end();
       }
-    })
-  }
+    });
 });
 
 server.listen(PORT, 'localhost', (error) => {
