2988db035cb038b769271c1ea5952d303653c396
Aleksander <sanyok.10rus@gmail.com>
2023-01-24 02:16:31 +0300

    added post-controller

--- routes/post-routes.js
+++ routes/post-routes.js
@@ -1,42 +1,14 @@
 const express = require('express');
+const {
+  getPost,
+  addPost,
+  deletePost
+} = require('../controllers/post-controller');
+
 const router = express.Router();
-const postsJSON = require('../data/posts.json');
-const writePost = require('../helpers/writePost');
-const deletePost = require('../helpers/deletePost');
-const createPath = require('../helpers/createPath');
 
-router.get('/posts/:id', (req, res) => {
-  const title = 'Post';
-  
-  const post = postsJSON.posts.find(el => el.id === req.params.id)
-  res.render(createPath('PostPage'), { title, post });
-});
-
-//POST//
-
-router.post('/add-post', (req, res) => {
-  const { title, author, text } = req.body;
-  const post = {
-    id: new Date(),
-    date: (new Date()).toLocaleDateString(),
-    title,
-    author,
-    text,
-  };
-  writePost(post);
-  res.render(createPath('PostPage'), { title, post });
-});
-
-//DELETE//
-
-router.delete('/posts/:id', (req, res) => {
-  const title = 'Post';
-  
-  deletePost(req.params.id);
-  
-  const posts = postsJSON.posts;
-
-  res.render(createPath('PostsPage'), { title, posts });
-});
+router.get('/posts/:id', getPost);
+router.post('/add-post', addPost);
+router.delete('/posts/:id', deletePost);
 
 module.exports = router;
